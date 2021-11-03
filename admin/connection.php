82afcc2c836c19fca3d8b9e0fed1c1f790db0af1
Digambar <digambarchaudhari425303@gmail.com>
2021-11-03 18:29:28 +0530

    Removed Extra Connection Code

--- admin/connection.php
+++ admin/connection.php
@@ -8,20 +8,6 @@
     define('DB_name', 'SkyBank');
     $conn = mysqli_connect(DB_host, DB_username, DB_password, DB_name);
 
-    // Defining constant php variable for 000.webhost
-    // define('DB_host', 'localhost');
-    // define('DB_username', 'id16592538_digambar');
-    // define('DB_password', 'uot8sjuuB*2buJ/~');
-    // define('DB_name', 'id16592538_skybank');
-    // $conn = mysqli_connect(DB_host, DB_username, DB_password, DB_name);
-
-    // Defining constant php variable for infinity host
-    // define('DB_host', 'localhost');
-    // define('DB_username', 'epiz_28120678');
-    // define('DB_password', 'TYTbq1Qh225ewF');
-    // define('DB_name', 'epiz_28120678_sky_bank');
-    // $conn = mysqli_connect(DB_host, DB_username, DB_password, DB_name);
-
     if (!$conn) {
         die("connection failed" . mysqli_connect_error());
          echo "Connection Fail";
